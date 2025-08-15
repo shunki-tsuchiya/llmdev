aa362c4dfccdbbd2f4759adfcab44519314b878d
shunki-tsuchiya <shtsuchi@lycorp.co.jp>
2025-08-15 17:51:10 +0900

    TODOリストの課題完了

--- 09_flask/todo/app.py
+++ 09_flask/todo/app.py
@@ -29,7 +29,10 @@ def index():
 
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
-    # 課題で実装します。
+    todos = load_todos()
+    if 0 <= todo_id < len(todos):
+        del todos[todo_id]
+        save_todos(todos)
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
